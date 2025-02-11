0df285228e9d3bcadc1c41ef2e23414890dbb9c0
[andrii kaliuha] <andriikaliuha.@gmail.com>
2025-02-11 20:55:33 +0200

    v0.62

--- src/components/SideMenu/Settings.tsx
+++ src/components/SideMenu/Settings.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 // import { SettingItem } from "./SettingItem";
 import { SettingsList } from "./SettingsList";
-import SettingStore from "../../store/main/SettingStore";
+// import SettingStore from "../../store/main/SettingStore";
 
 const Settings = observer(() => {
   // if (SettingStore.isSettingsItemVisible === true) return <SettingItem />;
