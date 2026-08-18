d2c448e866f1c852e3563be5557d1099583ef0a5
andrii kaliuha <andriikaliuha@gmail.com>
2026-08-18 16:47:41 +0300

    Revert "feat: og preview second try"
    This reverts commit 0c00064b08ea925f3b96e8d07ba93bd20e508223.

--- api/og.tsx
+++ api/og.tsx
@@ -1,5 +1,9 @@
 import { ImageResponse } from "@vercel/og";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default function handler(req: Request) {
   const { searchParams } = new URL(req.url);
   const theme = searchParams.get("theme") || "light";
